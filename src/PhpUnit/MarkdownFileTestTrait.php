7ce29380d0ab2d0ff3fcf8daad9719f3c67d2f0a
Petr Knap <8299754+petrknap@users.noreply.github.com>
2023-11-05 10:58:00 +0000

    feat: implemented PhpUnit\MarkdownFileTest

---
+++ src/PhpUnit/MarkdownFileTestTrait.php
@@ -0,0 +1,52 @@
+<?php declare(strict_types=1);
+
+namespace PetrKnap\Shorts\PhpUnit;
+
+use LogicException;
+use PetrKnap\Shorts\MarkdownShorts;
+use Traversable;
+
+trait MarkdownFileTestTrait
+{
+    /** @dataProvider dataPhpExamplesGivenInMarkdownFileWork */
+    public function testPhpExamplesGivenInMarkdownFileWork(string $example, string $expectedOutput): void
+    {
+        static::assertSame(
+            $expectedOutput,
+            MarkdownShorts::evaluatePhpExample($example)
+        );
+    }
+
+    public static function dataPhpExamplesGivenInMarkdownFileWork(): iterable
+    {
+        $pathToMarkdownFile = static::getPathToMarkdownFile();
+        $examples = MarkdownShorts::extractPhpExamples(file_get_contents($pathToMarkdownFile));
+        $expectedOutputs = static::getExpectedOutputsOfPhpExamples();
+
+        if ($expectedOutputs instanceof Traversable) {
+            $expectedOutputs = iterator_to_array($expectedOutputs);
+        }
+
+        foreach ($examples as $example) {
+            $expectedOutputId = array_key_first($expectedOutputs);
+            $expectedOutput = array_shift($expectedOutputs);
+
+            if ($expectedOutputId === null) {
+                throw new class ('Missing expect') extends LogicException implements MarkdownFileTestException {
+                };
+            }
+
+            yield "{$pathToMarkdownFile}#{$expectedOutputId}" => [$example, $expectedOutput];
+        }
+
+        if (!empty($expectedOutputs)) {
+            throw throw new class ('Missing example') extends LogicException implements MarkdownFileTestException {
+            };
+        }
+    }
+
+    public static function getExpectedOutputsOfPhpExamples(): iterable
+    {
+        return [];
+    }
+}
