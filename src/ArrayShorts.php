ff15518f0b8d9a1a8a06d8b2487daea7a119023f
Petr Knap <8299754+petrknap@users.noreply.github.com>
2023-11-05 09:15:25 +0000

    chore: updated PhpDoc

--- src/ArrayShorts.php
+++ src/ArrayShorts.php
@@ -3,23 +3,24 @@
 namespace PetrKnap\Shorts;
 
 /**
- * @template K The type of the individual keys
- * @template E The type of the individual elements
+ * @template K of int|string  The type of the individual keys
+ * @template E of mixed       The type of the individual elements
  */
 final class ArrayShorts
 {
     /**
-     * @param callable(?E, K): E $callback
-     * @param array<K,E> $array
+     * @param callable(?E, K): ?E $callback
+     * @param array<K, E> $array
+     * @param K $key
+     * @param K $keys
      *
-     * @return array<K,E>
+     * @return array<K, ?E>
      */
     public static function keyMap(callable $callback, array $array, mixed $key, mixed ...$keys): array
     {
         foreach (array_merge([$key], $keys) as $k) {
             $array[$k] = $callback($array[$k] ?? null, $k);
         }
-        /** @var array<K,E> $array */
         return $array;
     }
 }
