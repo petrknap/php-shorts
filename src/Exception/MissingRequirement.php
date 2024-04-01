d62978dc17c3b02621df018eb547578dbccccbd1
Petr Knap <8299754+petrknap@users.noreply.github.com>
2024-04-01 11:30:18 +0200

    feat: implemented `HasRequirements` interface

---
+++ src/Exception/MissingRequirement.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PetrKnap\Shorts\Exception;
+
+use LogicException;
+
+final class MissingRequirement extends LogicException implements ShortsException
+{
+    /**
+     * @param class-string $className
+     */
+    public function __construct(
+        string $className,
+        string $requirementType,
+        string $requirementName,
+    ) {
+        parent::__construct(
+            sprintf(
+                '%s requires %s %s',
+                $className,
+                $requirementType,
+                $requirementName,
+            )
+        );
+    }
+}
---
+++ src/Exception/ShortsException.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PetrKnap\Shorts\Exception;
+
+use Throwable;
+
+interface ShortsException extends Throwable
+{
+}
