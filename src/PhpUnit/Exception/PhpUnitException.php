da01517b8830220ae782b42cab6086ff7a127b22
Petr Knap <8299754+petrknap@users.noreply.github.com>
2024-04-04 17:48:55 +0200

    refactor: rearranged exceptions

---
+++ src/PhpUnit/Exception/PhpUnitException.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PetrKnap\Shorts\PhpUnit\Exception;
+
+use PetrKnap\Shorts\Exception\ShortsException;
+
+interface PhpUnitException extends ShortsException
+{
+}
