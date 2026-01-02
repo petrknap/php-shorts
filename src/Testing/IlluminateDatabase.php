9dc2df6e7545322f84cc2cb22515254c25c26f9e
Petr Knap <8299754+petrknap@users.noreply.github.com>
2026-01-02 22:46:17 +0100

    feat: added support for `Connection` to `IlluminateDatabase`
    fix: PDOs are now converted into platformrich connections

--- src/Testing/IlluminateDatabase.php
+++ src/Testing/IlluminateDatabase.php
@@ -6,7 +6,13 @@ namespace PetrKnap\Shorts\Testing;
 
 use Illuminate\Database\Capsule\Manager;
 use Illuminate\Database\Connection;
+use Illuminate\Database\Connectors\ConnectionFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\MySqlConnection;
+use Illuminate\Database\PostgresConnection;
+use Illuminate\Database\SQLiteConnection;
+use Illuminate\Database\SqlServerConnection;
+use InvalidArgumentException;
 use PDO;
 use PetrKnap\Shorts\HasRequirements;
 
@@ -24,25 +30,24 @@ final class IlluminateDatabase
     }
 
     /**
-     * @todo add support for PDO|array<string, string|PDO>|array<string, PDO|array<string, string|PDO>> $connections, PDO instance at self::CONNECTION_PDO_KEY
-     *
-     * @param PDO|array<string, PDO> $connections well-known PDOs keyed by names
+     * @param Connection|PDO|array<string, Connection|PDO> $connections well-known {@see Connection}s or/and {@see PDO}s keyed by names
      *
      * @return Manager
      *
      * @see Manager::setAsGlobal() if you want to use {@see Manager} globally
      * @see Manager::bootEloquent() if you want to use {@see Model}s
      */
-    public static function createCapsuleManager(PDO|array $connections): object
+    public static function createCapsuleManager(object|array $connections): object
     {
         self::checkRequirements(
             classes: [
-                Manager::class,
                 Connection::class,
+                Manager::class,
+                PDO::class,
             ],
         );
 
-        if ($connections instanceof PDO) {
+        if (!is_array($connections)) {
             $connections = [self::CONNECTION_DEFAULT_NAME => $connections];
         }
 
@@ -50,9 +55,42 @@ final class IlluminateDatabase
         foreach ($connections as $name => $connection) {
             $manager->addConnection([], $name);
             $manager->getDatabaseManager()
-                ->extend($name, static fn(): Connection => new Connection($connection));
+                ->extend($name, static fn(): Connection => $connection instanceof Connection
+                    ? $connection
+                    : self::createConnection($connection, $name));
         }
 
         return $manager;
+    }
+
+    /**
+     * @param PDO $pdo
+     *
+     * @return Connection
+     *
+     * @see ConnectionFactory::createConnection()
+     */
+    private static function createConnection(object $pdo, string $name): object
+    {
+        self::checkRequirements(
+            classes: [
+                MySqlConnection::class,
+                PDO::class,
+                PostgresConnection::class,
+                SQLiteConnection::class,
+                SqlServerConnection::class,
+            ],
+        );
+
+        $config = ['name' => $name];
+        /** @var string $driver */
+        $driver = $pdo->getAttribute(PDO::ATTR_DRIVER_NAME);
+        return match ($driver) {
+            'mysql' => new MySqlConnection($pdo, config: $config),
+            'pgsql' => new PostgresConnection($pdo, config: $config),
+            'sqlite' => new SQLiteConnection($pdo, config: $config),
+            'sqlsrv' => new SqlServerConnection($pdo, config: $config),
+            default => throw new InvalidArgumentException("Unsupported driver [{$driver}]."),
+        };
     }
 }
